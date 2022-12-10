f08d7bb44d3dd2c4c566b6f07c8491d451fb2089
Candace Hollinger <csig1985@gmail.com>
2022-12-10 19:08:36 +0000

    updated input functions

---
+++ Input Function.py
@@ -0,0 +1,11 @@
+#Input function for user turning it into a string
+input()
+input("My Name is Candace")
+
+#Message to display to the user
+input = favorite  = input("Favorite Color: Olive Green ")
+
+#Asking user for a few things with responses
+name = input("What is your name? ")
+color = input("What is yout favorite color?")
+age = int(input("How old are you today? "))
